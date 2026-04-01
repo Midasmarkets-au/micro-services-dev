5486d54bfebd663687383d2592473426de9aa246
Cody <xinsong.rao@edgeark.com.au>
2026-04-01 17:37:45 +1100

    fix  image issues

--- app/web/react/src/app/(protected)/sales/page.tsx
+++ app/web/react/src/app/(protected)/sales/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // import { useState, useEffect } from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
 // import { useTranslations } from 'next-intl';
 // import { useServerAction } from '@/hooks/useServerAction';
 // import { getSalesAccountStat } from '@/actions';
